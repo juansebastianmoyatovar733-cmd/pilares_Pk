3de21249ef4121f4bcc96fdc17196060a0480083
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-05 22:57:09 -0500

    Add AprendizSena class extending Empleado

---
+++ src/Ejercicio1/Ejercicio 2 /AprendizSena.java
@@ -0,0 +1,10 @@
+public class AprendizSena extends Empleado {
+    public AprendizSena(String nombre, String documento, double salarioBase) {
+        super(nombre, documento, salarioBase);
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salarioBase * 0.5;
+    }
+}
