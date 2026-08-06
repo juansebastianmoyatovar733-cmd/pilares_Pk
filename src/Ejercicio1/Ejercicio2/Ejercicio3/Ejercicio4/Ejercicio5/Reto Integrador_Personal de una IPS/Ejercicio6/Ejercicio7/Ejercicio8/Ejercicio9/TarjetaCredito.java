32b20b881a88fd12896d2eb414b9b43e4b1deaf2
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-06 10:36:54 -0500

    Create TarjetaCredito.java

---
+++ src/Ejercicio1/Ejercicio2/Ejercicio3/Ejercicio4/Ejercicio5/Reto Integrador_Personal de una IPS/Ejercicio6/Ejercicio7/Ejercicio8/Ejercicio9/TarjetaCredito.java
@@ -0,0 +1,15 @@
+public class TarjetaCredito extends MedioPago {
+    private String numeroTarjeta;
+
+    public TarjetaCredito(double monto, String numeroTarjeta) {
+        super(monto);
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    @Override
+    public void procesarPago() {
+        System.out.println("Validando tarjeta " + numeroTarjeta + "...");
+        System.out.println("Cobrando $" + monto + " a la tarjeta.");
+        System.out.println("Pago aprobado.");
+    }
+}
