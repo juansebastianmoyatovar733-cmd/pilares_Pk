4404bdfeca9d8c11ed895eba11e893a05d26c2fd
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-06 09:42:12 -0500

    Create Administrador.java

---
+++ src/Ejercicio1/Ejercicio2/Ejercicio3/Ejercicio4/Ejercicio5/Administrador.java
@@ -0,0 +1,14 @@
+public class Administrador extends Usuario {
+    private String nivelPermiso;
+
+    public Administrador(String nombre, String correo, String nivelPermiso) {
+        super(nombre, correo);
+        this.nivelPermiso = nivelPermiso;
+    }
+
+    @Override
+    public void iniciarSesion() {
+        super.iniciarSesion();
+        System.out.println("Panel de administracion con permisos: " + nivelPermiso);
+    }
+}
