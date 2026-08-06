604006a542edac32890daf89339ca4d779bce391
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-06 10:27:54 -0500

    Create MainFiguras.java

---
+++ src/Ejercicio1/Ejercicio2/Ejercicio3/Ejercicio4/Ejercicio5/Reto Integrador_Personal de una IPS/Ejercicio6/MainFiguras.java
@@ -0,0 +1,18 @@
+public class MainFiguras {
+    public static void main(String[] args) {
+        // Figura f = new Figura("X"); // ERROR: no se puede instanciar
+
+        Circulo c = new Circulo(5);
+        Triangulo t = new Triangulo(4, 6);
+        Rectangulo r = new Rectangulo(8, 3);
+        Cuadrado q = new Cuadrado(4);
+
+        c.mostrar();
+        System.out.println("---");
+        t.mostrar();
+        System.out.println("---");
+        r.mostrar();
+        System.out.println("---");
+        q.mostrar();
+    }
+}
