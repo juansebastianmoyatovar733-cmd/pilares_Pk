3617821b79965058f423a5a0104f4f4d00adb556
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-08 20:14:52 -0500

    Add Cuadrado class for area calculation

---
+++ src/2)Herencia/3)Abstraccion/cuadrado.java
@@ -0,0 +1,14 @@
+public class Cuadrado extends Figura {
+    private double lado;
+
+    public Cuadrado(double lado) {
+        super("Cuadrado");
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+}
+
