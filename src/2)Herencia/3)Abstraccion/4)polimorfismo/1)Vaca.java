abd5495b679920bd0eeafbe6d78de3236f115fb3
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-09 13:15:15 -0500

    Add Vaca class that extends Animal

---
+++ src/2)Herencia/3)Abstraccion/4)polimorfismo/1)Vaca.java
@@ -0,0 +1,10 @@
+public class Vaca extends Animal {
+    public Vaca(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(nombre + ": Muuu!");
+    }
+}
