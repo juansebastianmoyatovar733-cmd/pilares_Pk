18a76a47c7c7085b5e00d43e6a6d37a0bdcdd8dc
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-08 20:54:37 -0500

    Add EmpleadoFijo class for fixed salary employees

---
+++ src/2)Herencia/3)Abstraccion/2)Empleadofijo.JAVA
@@ -0,0 +1,13 @@
+public class EmpleadoFijo extends Empleado {
+    private double salarioMensual;
+
+    public EmpleadoFijo(String nombre, String documento, double salarioMensual) {
+        super(nombre, documento);
+        this.salarioMensual = salarioMensual;
+    }
+
+    @Override
+    public double calcularPago() {
+        return salarioMensual;
+    }
+}
