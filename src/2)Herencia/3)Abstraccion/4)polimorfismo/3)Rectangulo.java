f500c0b9a5aba0715d4d1db9c7189cd26200eef1
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-09 13:40:34 -0500

    Add Rectangulo class for area calculation

---
+++ src/2)Herencia/3)Abstraccion/4)polimorfismo/3)Rectangulo.java
@@ -0,0 +1,13 @@
+public class Rectangulo extends Figura {
+    private double ancho, alto;
+
+    public Rectangulo(double ancho, double alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    @Override
+    public double calcularArea() {
+        return ancho * alto;
+    }
+}
