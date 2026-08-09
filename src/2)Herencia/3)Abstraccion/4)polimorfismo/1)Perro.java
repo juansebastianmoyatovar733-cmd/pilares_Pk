adea80e6928e7c716f3654084ff57769c2fcd179
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-09 13:14:35 -0500

    Create Perro class that extends Animal

---
+++ src/2)Herencia/3)Abstraccion/4)polimorfismo/1)Perro.java
@@ -0,0 +1,10 @@
+public class Perro extends Animal {
+    public Perro(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(nombre + ": Guau guau!");
+    }
+}
