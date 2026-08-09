6e71f89d0682082349aabd3de5eaee6b80f179ca
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-09 13:41:27 -0500

    Create 4)Empleado.java

---
+++ src/2)Herencia/3)Abstraccion/4)polimorfismo/4)Empleado.java
@@ -0,0 +1,15 @@
+public class Empleado {
+    protected String nombre;
+
+    public Empleado(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double calcularSalario() {
+        return 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
