c3f2a3d189c23bfe4d4d1ad73069d036e20cc204
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-09 13:34:32 -0500

    Add Cuadrado class to calculate area of a square

---
+++ src/2)Herencia/3)Abstraccion/4)polimorfismo/3)Cuadrado.java
@@ -0,0 +1,12 @@
+public class Cuadrado extends Figura {
+    private double lado;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+}
