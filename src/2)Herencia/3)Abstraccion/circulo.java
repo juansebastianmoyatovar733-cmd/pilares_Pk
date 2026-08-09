c07a393c20f49dc2de3999b3e4b204b6ed10994f
SebasMoya <juansebastianmoyatovar733@gmail.com>
2026-08-08 19:28:40 -0500

    Add Circulo class to calculate area of a circle

---
+++ src/2)Herencia/3)Abstraccion/circulo.java
@@ -0,0 +1,13 @@
+public class Circulo extends Figura {
+    private double radio;
+
+    public Circulo(double radio) {
+        super("Circulo");
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+}
